921cf01808c2303174f8c6da0bde1c08e8984fff
Omar Dyab <81155341+Omardyab@users.noreply.github.com>
2021-08-01 02:08:02 +0300

    Revert "testing not finished yet"

--- python/code-challenges/Linked_list/Linked_list.py
+++ python/code-challenges/Linked_list/Linked_list.py
@@ -1,4 +1,3 @@
-
 class Node:
     def __init__(self, value):
         self.value = value
@@ -7,64 +6,50 @@ class Node:
 class Linkedlist:
     def __init__(self):
         self.head = None
-# """
-# define insert method which adds value in head
-# """
+        self.size = 0
+"""
+define insert method which adds value in head
+"""
     def insert(self, value='null'):
-        try:
-            first_node = Node(value)
-            if not self.head:
-                self.head = first_node
-            else:
-                current_node = self.head
-                self.head=first_node
-                self.head.next = current_node
-        except Exception as exception:
-            raise Exception(f"its not working as it should be: {exception}")
-# """
-# define include method which check if value exisited (in this case return false) otherwise return True
-# """
-    def includes(self, expectedvalue):
-        try:
-            current_node = self.head
-            while current_node:
-                if current_node.value == expectedvalue:
-                    return True
-                    break
-                current_node = current_node.next
-            return False
-        except Exception as exception:
-                raise Exception(f"its not working as it should be: {exception}")
-# """
-# Visual representation of the linked list bubbles
-# """
+        first_node = Node(value)
+        if !(self.head):
+            self.head = first_node
+        else:
+            current = self.head
+            self.head=first_node
+            self.head.next = current
+"""
+define include method which check if value exisited (in this case return false) otherwise return True
+"""
+def includes(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+                break
+            current = current.next
+        return False
+
+    """
+    visual representation of the linked list
+    """
     def __str__(self):
         result = ""
-        current_node = self.head
-        while current_node:
-            value = current_node.value
-            if current_node.next is None:
-                result =result+ f"({value})-> None"
+        current = self.head
+        while current:
+            value = current.value
+            if !(current.next):
+                result += f"({value})-> None"
                 break
             result = result + f"({value}) -> "
-            current_node=current_node.next
-        return result
+            current=current.next
+        return output
 
 if __name__ == "__main__":
 
     myll=Linkedlist()
-    print("my linked list now have : ",myll)
     myll.insert(1)
-    print("my linked list now have : ",myll)
-    myll.insert(2)
-    print("my linked list now have : ",myll)
-    myll.insert(7)
-    print("my linked list now have : ",myll)
-    myll.includes(7)
-    print("my linked list now have : ",myll)
-    myll.includes(9)
-    print("my linked list now have : ",myll)
-    print(myll)
+    print(myll.__str__)
 
 
 
--- python/code-challenges/Linked_list/tests/test_linked_list.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from Linked_list import __version__
-from Linked_list.Linked_list import *
-
-def test_version():
-    assert __version__ == '0.1.0'
-
-
-# testing part is incomplete
