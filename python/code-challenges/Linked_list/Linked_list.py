9d7dac42acab661ebf27bd273edf83bb16ff9e87
Omardyab <omarzadyab@gmail.com>
2021-08-02 23:54:31 +0300

    code ch 7

--- python/code-challenges/Linked_list/Linked_list.py
+++ python/code-challenges/Linked_list/Linked_list.py
@@ -48,20 +48,51 @@ class Linkedlist:
             result = result + f"({value}) -> "
             current_node=current_node.next
         return result
+
+    def knth_from_end(self, k_value):
+        current_node = self.head
+        len_list=len(self)-1
+        nodes_loop=(len_list-k_value)-1
+        try:
+            if current_node==None:
+                return ("the k value is None")
+            if k_value<=-1 or k_value>len_list:
+                return ("this is not a positive value or the value you have is out of the given range")
+            # while nodes_loop>0:
+            #     current_node = current_node.next
+            #     nodes_loop=nodes_loop-1
+            # print(nodes_loop)
+            # return current_node.value
+        except Exception as exception:
+                raise Exception(f"its not working as it should be: {exception}")
+
+
+
 if __name__ == "__main__":
-    myll=Linkedlist()
-    print("my linked list now have : ",myll)
-    myll.insert(1)
-    print("my linked list now have : ",myll)
-    myll.insert(2)
-    print("my linked list now have : ",myll)
-    myll.insert(7)
-    print("my linked list now have : ",myll)
-    myll.includes(7)
-    print("my linked list now have : ",myll)
-    myll.includes(9)
-    print("my linked list now have : ",myll)
-    print(myll)
+    # myll=Linkedlist()
+    # print("my linked list now have : ",myll)
+    # myll.insert(1)
+    # print("my linked list now have : ",myll)
+    # myll.insert(2)
+    # print("my linked list now have : ",myll)
+    # myll.insert(7)
+    # print("my linked list now have : ",myll)
+    # myll.includes(7)
+    # print("my linked list now have : ",myll)
+    # myll.includes(9)
+    # print("my linked list now have : ",myll)
+    # print(myll)
+
+    ll=Linkedlist()
+    ll.insert(1)
+    ll.insert(3)
+    ll.insert(8)
+    ll.insert(2)
+    print(ll)
+    print(ll.knth_from_end(-1))
+    print(ll.knth_from_end(8))
+    print(ll.knth_from_end(3))
+    print(ll.knth_from_end(-44))
 
 
 
