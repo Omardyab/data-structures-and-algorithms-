710c12e5bbb6d17285579b972f52b31b3bad5495
Omardyab <omarzadyab@gmail.com>
2021-08-03 19:40:43 +0300

    tests are added

--- python/code-challenges/Linked_list/tests/test_linked_list.py
+++ python/code-challenges/Linked_list/tests/test_linked_list.py
@@ -4,19 +4,21 @@ from Linked_list import *
 
 # def test_version():
 #     assert __version__ == '0.1.0'
-
 def test_empty_l_list():
-    emptylist=Linkedlist()
+    emptylist=Linkedlist().head
     actual=emptylist
-    expected=''
+    expected=None
     assert actual==expected
 
 def test_insert():
     myll = Linkedlist()
     myll.insert(12)
     actual =myll.head.value
+    actualnext=myll.head.next
     expected=12
+    expectednext=None
     assert actual==expected
+    assert actualnext==expectednext
 
 def test_linked_states(myll):
     assert str(myll) == f"(6) -> (54) -> (100)-> None"
@@ -28,6 +30,12 @@ def test_includes(myll):
 def test_head_node(myll):
     assert myll.head.value == 6
     assert myll.head.next.value == 54
+def test_falsy_values():
+    myll2=Linkedlist()
+    myll2.insert('12')
+    myll2.insert('212')
+    actual=myll2.includes('2121')
+    assert actual==False
 
 @pytest.fixture
 def myll():
