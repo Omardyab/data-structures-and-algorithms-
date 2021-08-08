b55c7c3fab32de6998e04fb2e29c478cdde5d5ef
Omardyab <omarzadyab@gmail.com>
2021-08-08 03:55:34 +0300

    movig due to conflict

--- python/code-challenges/stack_and_queue/test_stack_and_queue.py
+++ python/code-challenges/stack_and_queue/test_stack_and_queue.py
@@ -1,6 +1,5 @@
 from stack_and_queue import *
 import pytest
-
 """ Test cases for Stack
     Can successfully push onto a stack
     Can successfully push multiple values onto a stack
@@ -13,20 +12,17 @@ def test_stack_push_one():
     stack = Stack()
     stack.push(100)
     assert stack.top.value == 100
-
 def test_stack_push_multiple():
     stack = Stack()
     stack.push(100)
     stack.push(200)
     assert stack.top.value == 200
-
 def test_stack_pop():
     stack = Stack()
     stack.push(100)
     stack.push(200)
     stack.pop()
     assert stack.top.value == 100
-
 def test_stack_empty():
     stack = Stack()
     stack.push(100)
@@ -34,25 +30,21 @@ def test_stack_empty():
     stack.pop()
     stack.pop()
     assert stack.isEmpty() == True
-
 def test_stack_peek():
     stack = Stack()
     stack.push(400)
     stack.push(600)
     assert stack.peek() == 600
-
 # instantiate
 def test_stack_instantiate():
     stack = Stack()
     assert stack.top == None
-
 def test_stack_empty_exception():
     stack = Stack()
     with pytest.raises(Exception):
         assert stack.peek()
     with pytest.raises(Exception):
         assert stack.pop()
-
 """ Test cases for Queue
     Can successfully enqueue into a queue
     Can successfully enqueue multiple values into a queue
@@ -65,27 +57,23 @@ def test_enqueue_one():
     queue = Queue()
     queue.enqueue(700)
     assert queue.rear.value == 700
-
 def test_enqueue_multiple():
     queue = Queue()
     queue.enqueue(700)
     queue.enqueue(800)
     assert queue.rear.value == 800
     assert queue.front.value == 700
-
 def test_dequeue():
     queue = Queue()
     queue.enqueue(700)
     queue.dequeue()
     queue.enqueue(800)
     assert queue.front.value == 800
-
 def test_queue_peek():
     queue = Queue()
     queue.enqueue("Omar")
     queue.enqueue("Nawras")
     assert queue.peek() == "Omar"
-
 def test_is_empty():
     queue = Queue()
     queue.enqueue("Omar")
@@ -93,12 +81,10 @@ def test_is_empty():
     queue.dequeue()
     queue.dequeue()
     assert queue.isEmpty() == True
-
 def test_instantiate():
     queue = Queue()
     assert queue.front == None
     assert queue.rear == None
-
 def test_queue_empty_exception():
     queue = Queue()
     with pytest.raises(Exception):
